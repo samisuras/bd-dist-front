9aa028588856c1d8fdbc56f867a059438659a9c9
samisuras <raulsamuelleos98@gmail.com>
2020-03-23 14:10:14 -0600

    Estructura base de roles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { EstudianteComponent } from './components/usuarios/estudiante/estudiante.component';
+import { MaestroComponent } from './components/usuarios/estudiante/maestro/maestro.component';
+import { LaboratoristaComponent } from './components/usuarios/laboratorista/laboratorista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    EstudianteComponent,
+    MaestroComponent,
+    LaboratoristaComponent
   ],
   imports: [
     BrowserModule,

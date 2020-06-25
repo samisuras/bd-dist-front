47e03e4fa036ee92887c37a200b20d7b81b69d4f
samisuras <raulsamuelleos98@gmail.com>
2020-06-25 00:00:23 -0500

    arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,11 +25,8 @@ import { VerExamenesComponent } from './components/usuarios/estudiante/ver-exame
 import { VerPracticaComponent } from './components/usuarios/estudiante/ver-practica/ver-practica.component';
 import { RealizarExamenComponent } from './components/usuarios/estudiante/realizar-examen/realizar-examen.component';
 import { PrestamosComponent } from './components/usuarios/laboratorista/prestamos/prestamos.component';
-<<<<<<< HEAD
 import { SubirPracticaComponent } from './components/usuarios/estudiante/subir-practica/subir-practica.component';
-=======
 import { CrearPrestamoComponent } from './components/usuarios/laboratorista/crear-prestamo/crear-prestamo.component';
->>>>>>> 2bd52b2d81349bc6a0a68755a897a738d657e29f
 
 @NgModule({
   declarations: [
@@ -54,11 +51,8 @@ import { CrearPrestamoComponent } from './components/usuarios/laboratorista/crea
     VerPracticaComponent,
     RealizarExamenComponent,
     PrestamosComponent,
-<<<<<<< HEAD
     SubirPracticaComponent
-=======
     CrearPrestamoComponent
->>>>>>> 2bd52b2d81349bc6a0a68755a897a738d657e29f
   ],
   imports: [
     BrowserModule,
